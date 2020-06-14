0438fd6402c92a80ab4e01f3a11c60233b79d010
Neissa Dorsinville <neissa@dorsinville.com>
2020-06-14 19:33:33 -0400

    ShopContainer added

---
+++ src/components/ShopContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ShopContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h2> Number of Cakes </h2>
+        <button> Buy Cakes </button>
+      </div>
+    );
+  }
+}
+
+export default ShopContainer;
